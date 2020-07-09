a18ed9df380b52496a4981233cc0f2d9a5f50ea1
unknown <severo_vasquez97@yahoo.com>
2020-07-08 20:56:55 -0500

    Called API for contacts

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
-
-
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-     ]
+    contacts: []
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll()
+    .then((contacts) => {
+      this.setState(() => ({
+        contacts
+      }))
+    })
   }
   removeContact = (contact) => {
     this.setState((currentState) => ({
@@ -33,6 +22,7 @@ class App extends Component {
       })
     }))
   }
+
   render() {
     return (
       <div>
